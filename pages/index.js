fd077eab737699034b4d82e97037911681c65fd1
VictorAlmeidaa <victordealmeida5@gmail.com>
2026-01-21 12:46:03 -0300

    modified pages/index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Luene, eu amo você, se você me ama, dá uma risadinha! ❤🥰</h1>;
+  return <h1>Luene, eu amo você. se você me ama, dá uma risadinha! ❤🥰</h1>;
 }
 
 export default Home;
