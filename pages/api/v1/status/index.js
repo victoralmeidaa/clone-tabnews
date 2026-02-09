5295dcb2e3350188bcac2c63f63eaf448127ddbb
VictorAlmeidaa <victordealmeida5@gmail.com>
2026-02-08 23:48:27 -0300

    modified status/index.js file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,8 @@
-import database from "../../../../infra/database";
+import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as Soma;");
-  console.log(result);
+  const result = await database.query("SELECT 1 + 3 as Soma;");
+  console.log("Soma: " + result.rows[0].soma);
   response.status(200).json({ chave: "ok - não tem nada de errado" });
 }
 
